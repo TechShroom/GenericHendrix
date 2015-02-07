38306de7a237097d1739f807f5b005e215ee5c33
Kenzie Togami <ket1999@gmail.com>
2015-02-07 09:49:41 -0800

    Use Guava

--- src/main/java/com/techshroom/hendrix/Util.java
+++ src/main/java/com/techshroom/hendrix/Util.java
@@ -1,6 +1,9 @@
 package com.techshroom.hendrix;
 
 import static com.google.common.base.Verify.verify;
+
+import com.google.common.io.BaseEncoding;
+
 import fj.data.Array;
 
 /**
@@ -9,8 +12,6 @@ import fj.data.Array;
  * @author Kenzie Togami
  */
 public final class Util {
-    private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
-
     /**
      * Convert a byte array to a hex string.
      * 
@@ -18,13 +19,7 @@ public final class Util {
      * @return A hex string representing the bytes
      */
     public static String bytesToHex(byte[] bytes) {
-        char[] hexChars = new char[bytes.length * 2];
-        for (int j = 0; j < bytes.length; j++) {
-            int v = bytes[j] & 0xFF;
-            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
-            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
-        }
-        return String.copyValueOf(hexChars);
+        return BaseEncoding.base16().encode(bytes);
     }
 
     /**
